4a97486cbc6000a23ce3475fa6707df0f83e909a
alex1606-alt <zubator777@gmail.com>
2025-01-15 13:45:43 +0100

    2

---
+++ project/script.js
@@ -0,0 +1,7 @@
+let productCount=10;
+let productName="Milk";
+productCount=15;
+productName="Juise";
+console.log(productCount,productName);
+document.getElementById("zmin").innerHTML= productCount +","+productName;
+
