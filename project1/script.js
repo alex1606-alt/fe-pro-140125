10dbbb9a6554f70662579f0e81608626d379bb10
alex1606-alt <zubator777@gmail.com>
2025-01-17 10:06:58 +0100

    1

---
+++ project1/script.js
@@ -0,0 +1,58 @@
+console.log(`${(5+3)*2-7}`);
+console.log((10 / 2) ** 2);
+console.log(2 * (4 + 3) - (8 / 2));
+console.log((15 % 4) + 5);
+console.log(3 - (2 ** 4));
+let num1=10;
+let num2=5;
+console.log(`${num1}+${num2}`);
+console.log(`${num1 + num2}`);
+
+
+let number1=7;
+let number2=5;
+let real=(number1==number2);
+console.log(real);
+alert(real);
+
+let zm = prompt("введите число");
+let fag=(zm<6);
+console.log(zm);
+console.log(fag);
+if (zm % 2 === 0){console.log(`число является парным`)} 
+else{ console.log(`число является непарным`)};
+
+
+
+let age = prompt("ваш возраст");
+if(age<18){alert("вы не можете користуватись ресурсом")}
+else{alert("користуйтесь будь ласка")};
+
+
+
+
+let pric = prompt("цена товара");
+let quan = prompt("кілька штук");
+let pay = pric*quan;
+alert(`${pay} долларов`);
+console.log(`стоимость общая ${pay} долларов`);
+
+
+let tov = prompt("на яку вартість ви придбали товар");
+let discount = (tov/2);
+alert(`ваша знижка дорівнює ${discount} грн`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
