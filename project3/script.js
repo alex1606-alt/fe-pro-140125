86d82890500543737ebd79423853e09aa545fe69
alex1606-alt <zubator777@gmail.com>
2025-01-24 14:42:05 +0100

    1

---
+++ project3/script.js
@@ -0,0 +1,51 @@
+
+//function calculator (x,y,operator){
+    //switch(operator){
+        //case "+":alert(`${x+y}`)
+        //break;
+        //case "-":alert(`${x-y}`)
+        //break;
+        //case "*":alert(`${x*y}`)
+        //break;
+       // case "/":alert(`${x/y}`)
+       // break;
+
+
+
+    //}
+//};
+//calculator(2,5,"*");
+
+
+
+
+
+//function find (){
+   // for(i=1;i<11;i++){if(i%2==0){alert(i); break}};
+
+//};
+//find();
+
+
+
+
+//function finde (){
+    //for(i=1;i<11;i++){if(i%2!==0){continue;}
+        //alert(i); }};
+
+
+//finde();
+
+
+
+
+let gerArray =[
+    ["tom","email",24],
+    ["bob","email",33],
+    ["ken","email",44],
+];
+for(i=0;i<gerArray.length;i++){
+    for(c=0;c<gerArray[i].length;c++){console.log(`${gerArray[i][c]}`)}
+
+};
+
