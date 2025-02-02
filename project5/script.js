bcbc83befc27532fa368f75b0a220a24a024d4cf
alex1606-alt <zubator777@gmail.com>
2025-02-02 18:17:37 +0100

    33

--- project5/script.js
+++ project5/script.js
@@ -20,3 +20,44 @@ emp2.sayHello();
 
 
 
+
+
+class Student extends Person{
+    constructor(name,age,studenit){
+        super(name,age)
+        this._studenit = studenit;
+        
+    };
+     study(){if(this._studenit == "yes"){
+        console.log(`зараховано ${this._name}`)}
+        else{console.log(`відраховано ${this._name}`)}
+     }
+
+};
+let x = new Student("KOLIA",18,"no");
+console.log(x);
+x.study();
+
+
+
+
+
+
+function count(){
+let y = prompt("скільки вам років?");
+let currentYear = new Date().getFullYear();
+let z = currentYear-y;
+alert(`${z} рік`);
+
+};
+count();
+
+
+
+
+
+
+
+
+
+
